e4b20f05cd7ed7de9ec719000ce8aaba34e575e4
grigory <grigory@goodnotesapp.com>
2022-11-11 01:51:26 +0700

    clean up

--- src/main/kotlin/tga/gaming/game/zombie/Main.kt
+++ src/main/kotlin/tga/gaming/game/zombie/Main.kt
@@ -2,7 +2,6 @@ package tga.gaming.game.zombie
 
 import kotlinx.browser.document
 import kotlinx.browser.window
-import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.Node
 import tga.gaming.engine.model.v
@@ -28,9 +27,8 @@ fun main() {
 
 fun Node.initCanvas(): HTMLCanvasElement {
     val canvas = document.createElement("canvas") as HTMLCanvasElement
-    val context = canvas.getContext("2d") as CanvasRenderingContext2D
-    context.canvas.width  = window.innerWidth - 20
-    context.canvas.height = window.innerHeight - 20
+    canvas.width  = window.innerWidth - 20
+    canvas.height = window.innerHeight - 20
     appendChild(canvas)
 
     return canvas
