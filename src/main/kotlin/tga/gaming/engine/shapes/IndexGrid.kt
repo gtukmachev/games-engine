258122f18b194831a15c8e02b9abbacf1da25b54
grigory <grigory@goodnotesapp.com>
2022-11-13 20:50:20 +0700

    + Composite drawer

--- src/main/kotlin/tga/gaming/engine/shapes/IndexGrid.kt
+++ src/main/kotlin/tga/gaming/engine/shapes/IndexGrid.kt
@@ -1,6 +1,8 @@
 package tga.gaming.engine.shapes
 
 import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.CanvasTextBaseline
+import org.w3c.dom.TOP
 import tga.gaming.engine.index.sizeFactor
 import tga.gaming.engine.model.Drawable
 import tga.gaming.engine.model.Obj
@@ -16,12 +18,15 @@ class IndexGrid : Obj(frame = null), Drawable {
             var x = 0.0
             for (c in 0 until index.columns) {
                 ctx.beginPath()
-                ctx.strokeStyle = if (index.matrix[l][c].size > 0) {
-                                        "orange"
-                                  } else {
-                                        "gray"
-                                  }
-                ctx.rect(x+2,y+2, size-4,size-4)
+                val count = index.matrix[l][c].size
+                ctx.strokeStyle = if ( count > 0) "gray" else "black"
+
+                val x0 = x+2
+                val y0 = y+2
+
+                ctx.strokeText("$count",x0+3,y0+3)
+                ctx.textBaseline = CanvasTextBaseline.TOP
+                ctx.rect(x0,y0, size-4,size-4)
                 ctx.stroke()
                 x += size
             }
